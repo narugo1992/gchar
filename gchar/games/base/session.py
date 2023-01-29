b50dde55a1ab9da412bee68370ade1120c779a85
narugo1992 <narugo@126.com>
2023-01-29 20:23:18 +0800

    dev(narugo): try fix the scripts

--- gchar/games/base/session.py
+++ gchar/games/base/session.py
@@ -2,17 +2,37 @@ from typing import Optional, Dict
 
 import requests
 from requests.adapters import HTTPAdapter, Retry
-from requests.status_codes import codes as _http_status_codes
 
-_5xx = [val for val in _http_status_codes.keys() if val // 100 == 5]
+DEFAULT_TIMEOUT = 10  # seconds
 
 
-def get_requests_session(max_retries: int = 3, headers: Optional[Dict[str, str]] = None) -> requests.Session:
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.timeout = DEFAULT_TIMEOUT
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+        return super().send(request, **kwargs)
+
+
+def get_requests_session(max_retries: int = 5, timeout: int = DEFAULT_TIMEOUT,
+                         headers: Optional[Dict[str, str]] = None) -> requests.Session:
     session = requests.session()
-    retries = Retry(total=max_retries, backoff_factor=1,
-                    status_forcelist=[500, 501, 502, 503, 504, 505, 506, 507, 509, 510, 511])
-    session.mount('http://', HTTPAdapter(max_retries=retries))
-    session.mount('https://', HTTPAdapter(max_retries=retries))
+    retries = Retry(
+        total=max_retries, backoff_factor=1,
+        # status_forcelist=[500, 501, 502, 503, 504, 505, 506, 507, 509, 510, 511],
+        status_forcelist=[429, 500, 502, 503, 504],
+        method_whitelist=["HEAD", "GET", "OPTIONS"],
+    )
+    adapter = TimeoutHTTPAdapter(max_retries=retries, timeout=timeout)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     session.headers.update({
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                       "(KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
