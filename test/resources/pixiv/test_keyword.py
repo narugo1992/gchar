9727b730898e385be602cba85609ab23ce6ad686
narugo1992 <narugo@126.com>
2023-02-10 20:08:34 +0800

    dev(narugo): save this

--- test/resources/pixiv/test_keyword.py
+++ test/resources/pixiv/test_keyword.py
@@ -1,4 +1,5 @@
 import os
+import time
 from unittest import skipUnless
 
 import pytest
@@ -27,9 +28,25 @@ class TestResourcesPixivKeyword:
 
     @skipUnless(os.environ.get(REMOTE_PIXIV_SESSION_URL, None), 'Pixiv token required.')
     def test_get_by_keyword(self, ch_amiya, ch_slzd):
-        with pytest.warns(UserWarning):
-            cnt = get_pixiv_illustration_count_by_keyword(ch_amiya)
-        assert 12000 <= cnt <= 20000
+        for i in range(5):
+            with pytest.warns(UserWarning):
+                cnt = get_pixiv_illustration_count_by_keyword(ch_amiya)
+            if cnt == 0:
+                time.sleep(10.0)
+                continue
+            else:
+                assert 12000 <= cnt <= 20000
+                break
+        else:
+            assert False, f'Max try exceed for pixiv api - {ch_amiya!r}.'
 
-        cnt = get_pixiv_illustration_count_by_keyword('アークナイツ')
-        assert 180000 <= cnt <= 300000
+        for i in range(5):
+            cnt = get_pixiv_illustration_count_by_keyword('アークナイツ')
+            if cnt == 0:
+                time.sleep(10.0)
+                continue
+            else:
+                assert 180000 <= cnt <= 300000
+                break
+        else:
+            assert False, 'Max try exceed for pixiv api - \'アークナイツ\'.'
