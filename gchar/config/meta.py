84b707c280213a57f678f1a4d5b8c049939d738f
narugo1992 <narugo@126.com>
2023-02-20 12:44:57 +0800

    release(narugo): use version 0.0.6

--- gchar/config/meta.py
+++ gchar/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = 'gchar'
 
 #: Version of this project.
-__VERSION__ = '0.0.5'
+__VERSION__ = '0.0.6'
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Game character manager.'
