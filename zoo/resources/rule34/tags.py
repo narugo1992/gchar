d8b3b488023326a425fc0b187fa23d7f3bc8a43f
narugo1992 <narugo@126.com>
2023-07-04 01:54:03 +0800

    dev(narugo): add new site based on rule34, ci skip

--- zoo/resources/rule34/tags.py
+++ zoo/resources/rule34/tags.py
@@ -1,5 +1,6 @@
 import json
 import sqlite3
+from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
 import xmltodict
@@ -12,27 +13,48 @@ def crawl_tags_to_json(site_root: str = 'https://rule34.xxx'):
     session = get_requests_session(headers={
         'Content-Type': 'application/json; charset=utf-8',
     })
-    data, exist_ids = [], set()
-    pg = tqdm()
 
-    page_no = 0
-    while True:
+    def _crawl(p):
         resp = srequest(session, 'GET', f'{site_root}/index.php', params={
             'page': 'dapi',
             's': 'tag',
             'q': 'index',
             'json': '1',
             'limit': '100',
-            'pid': str(page_no),
+            'pid': str(p),
         })
         resp.raise_for_status()
 
         json_data = xmltodict.parse(resp.text)
         if 'tags' not in json_data or 'tag' not in json_data['tags']:
+            return None
+        return json_data['tags']['tag']
+
+    l, r = 1, 2
+    while True:
+        if _crawl(r):
+            l, r = l << 1, r << 1
+            print(f'Left: {l}, right: {r}')
+        else:
             break
-        tags = json_data['tags']['tag']
+
+    while l < r:
+        m = (l + r + 1) // 2
+        if _crawl(m):
+            l = m
+        else:
+            r = m - 1
+        print(f'Left: {l}, right: {r}')
+
+    pages = l
+    data, exist_ids = [], set()
+    pg_page = tqdm(total=pages, desc='Pages')
+    pg_tags = tqdm(desc='Tags')
+
+    def _process(p):
+        tags = _crawl(p)
         if not tags:
-            break
+            return
 
         for item in tags:
             item = {key.lstrip('@'): value for key, value in item.items()}
@@ -45,9 +67,15 @@ def crawl_tags_to_json(site_root: str = 'https://rule34.xxx'):
 
             data.append(item)
             exist_ids.add(item['id'])
+            pg_tags.update()
 
-        page_no += 1
-        pg.update()
+        pg_page.update()
+
+    tp = ThreadPoolExecutor(max_workers=12)
+    for i in range(0, pages + 1):
+        tp.submit(_process, i)
+
+    tp.shutdown()
 
     data = sorted(data, key=lambda x: -x['id'])
     return data
