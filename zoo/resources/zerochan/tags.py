57b8afb850fe2c8d9eeb9af5c19e81e535b548f8
narugo1992 <narugo@126.com>
2023-07-04 13:46:10 +0800

    dev(narugo): use new framework to crawl konachan, ci skip

--- zoo/resources/zerochan/tags.py
+++ zoo/resources/zerochan/tags.py
@@ -1,33 +1,32 @@
-import sqlite3
-import time
+from typing import Optional, List, Mapping, Any
 
-import pandas as pd
 from pyquery import PyQuery as pq
-from tqdm.auto import tqdm
 
-from gchar.utils import get_requests_session, srequest
+from gchar.utils import srequest
+from ..base.tags import ParallelTagCrawler
 
 
-def crawl_tags_to_json(min_timespan: float = 0.6):
-    session = get_requests_session(headers={'User-Agent': 'Tag Crawler - narugo1992'})
-    page_no = 1
-    data, exist_names = [], set()
-    pg = tqdm()
-    _last_time = time.time()
+class ZerochanTagCrawler(ParallelTagCrawler):
+    __max_workers__ = 4
+    __id_key__ = 'tag'
+    __init_page__ = 1
 
-    while True:
-        _last_time += min_timespan
-        time.sleep(max(_last_time - time.time(), 0.0))
-        resp = srequest(session, 'GET', 'https://www.zerochan.net/tags', params={
+    def __init__(self, site_url: str = 'https://zerochan.net'):
+        ParallelTagCrawler.__init__(self, site_url)
+        self.session.headers.update({'User-Agent': 'Tag Crawler - narugo1992'})
+
+    def get_tags_from_page(self, p, **kwargs) -> Optional[List[Mapping[str, Any]]]:
+        resp = srequest(self.session, 'GET', f'{self.site_url}/tags', params={
             's': 'count',
             'm': 'details',
             'q': '',
             't': '',
-            'p': str(page_no)
+            'p': str(p)
         })
         resp.raise_for_status()
         page = pq(resp.text)
-        active = False
+
+        data = []
         for row in page('table.tags tr').items():
             if len(row('td')) == 0:
                 continue
@@ -40,48 +39,16 @@ def crawl_tags_to_json(min_timespan: float = 0.6):
             children_count = int(row('td:nth-child(5)').text().replace(',', '').strip())
             parent_count = int(row('td:nth-child(6)').text().replace(',', '').strip())
 
-            if name not in exist_names:
-                data.append({
-                    'tag': name,
-                    'type': type_,
-                    'parent': parent,
-                    'total': total,
-                    'strict': strict,
-                    'children_count': children_count,
-                    'parent_count': parent_count,
-                })
-                exist_names.add(name)
+            data.append({
+                'tag': name,
+                'type': type_,
+                'parent': parent,
+                'total': total,
+                'strict': strict,
+                'children_count': children_count,
+                'parent_count': parent_count,
+            })
 
-            active = True
+        return data
 
-        if not active:
-            break
-
-        page_no += 1
-        pg.update()
-
-    return data
-
-
-def json_to_df(json_):
-    df = pd.DataFrame(json_).astype({})
-    return df
-
-
-def json_save_to_csv(json_, csv_file):
-    df = json_to_df(json_)
-    df.to_csv(csv_file, index=False)
-    return csv_file
-
-
-def json_save_to_sqlite(json_, sqlite_file):
-    sql = sqlite3.connect(sqlite_file)
-    df = json_to_df(json_)
-    df.to_sql('tags', sql, )
-
-    index_columns = ['children_count', 'parent', 'parent_count', 'strict', 'tag', 'total', 'type']
-    for column in index_columns:
-        sql.execute(f"CREATE INDEX tags_index_{column} ON tags ({column});").fetchall()
-
-    sql.close()
-    return sqlite_file
+    __sqlite_indices__ = ['children_count', 'parent', 'parent_count', 'strict', 'tag', 'total', 'type']
