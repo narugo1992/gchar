151663084d3b4e2817ff7ce9eed13132aa4a7cee
narugo1992 <narugo@126.com>
2023-07-06 17:16:17 +0800

    dev(narugo): add aliases

--- zoo/resources/gelbooru/tags.py
+++ zoo/resources/gelbooru/tags.py
@@ -1,8 +1,9 @@
 import re
 from itertools import chain
-from typing import Optional, List, Mapping, Any
+from typing import Optional, List, Mapping, Any, Tuple
 
 from pyquery import PyQuery as pq
+from tqdm.auto import tqdm
 
 from gchar.utils import srequest
 from ..rule34.tags import ParallelTagCrawler
@@ -16,11 +17,47 @@ class GelbooruTagCrawler(ParallelTagCrawler):
     def __init__(self):
         ParallelTagCrawler.__init__(self, 'https://gelbooru.com')
 
-    # def get_tag_aliases_from_page(self, p, **kwargs) -> List[Tuple[str, str]]:
-    #     pass
-    #
-    # def get_tag_aliases_json(self) -> List[Tuple[str, str]]:
-    #     pass
+    def _get_tag_aliases_from_page(self, p, **kwargs) -> List[Tuple[str, str]]:
+        resp = srequest(self.session, 'GET', f'{self.site_url}/index.php', params={
+            'page': 'alias',
+            's': 'list',
+            'pid': str((p - 1) * 50),
+        })
+        resp.raise_for_status()
+
+        page = pq(resp.text)
+
+        page_pg = page('#paginator .pagination')
+        paginator_words = set([
+            item.text().strip()
+            for item in chain(page_pg('a').items(), page_pg('b').items())
+        ])
+        if str(p) not in paginator_words:
+            return []
+
+        table = page('#aliases table')
+
+        data = []
+        for row in table('tr').items():
+            if len(list(row('td').items())) == 0:
+                continue
+
+            first_a, second_a = row('a').items()
+            alias_tag = first_a.text().strip()
+            tag = second_a.text().strip()
+            data.append((alias_tag, tag))
+
+        return data
+
+    def get_tag_aliases_json(self) -> List[Tuple[str, str]]:
+        data, exist_ids = [], set()
+        pg_tags = tqdm(desc=f'Tag Aliases')
+        pg_pages = tqdm(desc=f'Pages')
+        return self._load_data_with_pages(
+            self._get_tag_aliases_from_page,
+            lambda x: x[0],
+            data, exist_ids, pg_pages, pg_tags,
+        )
 
     def get_tags_from_page(self, p, **kwargs) -> Optional[List[Mapping[str, Any]]]:
         resp = srequest(self.session, 'GET', f'{self.site_url}/index.php', params={
@@ -64,4 +101,4 @@ class GelbooruTagCrawler(ParallelTagCrawler):
 
         return data
 
-    __sqlite_indices__ = ['name', 'count', 'type']
+    __sqlite_indices__ = ['name', 'count', 'type', 'is_ambiguous']
