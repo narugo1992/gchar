6fb41872a669e04eca9fdeb3ba85950c2a74f96e
narugo1992 <narugo@126.com>
2023-07-07 21:45:46 +0800

    dev(narugo): optimize search regex

--- zoo/resources/base/tag_matches.py
+++ zoo/resources/base/tag_matches.py
@@ -78,7 +78,7 @@ class TagMatcher(HuggingfaceDeployable):
     __count_column__: str = 'count'
     __extra_filters__: dict = {'type': 4}
     __case_insensitive__: bool = False
-    __min_similarity__: float = 0.6
+    __min_similarity__: float = 0.7
     __strict_similarity__: float = 0.95
     __yes_min_vsim__: float = 0.60
     __no_max_vsim__: float = 0.20
@@ -234,16 +234,22 @@ class TagMatcher(HuggingfaceDeployable):
     def _iter_patterns_by_name_words(self, name_words_sets: List[List[str]]) -> Iterator[str]:
         # name without keyword
         for name_words in name_words_sets:
-            for word_cmbs in itertools.combinations(name_words, max(len(name_words) // 2, 1)):
-                for words in itertools.permutations(word_cmbs):
-                    origin_text = '_'.join(words)
-                    for i in range(len(origin_text)):
-                        new_text = ''.join([
-                            '_' if i == j else origin_text[j]
-                            for j in range(len(origin_text))
-                        ])
-                        pattern = '%'.join(['', *new_text.split('_'), ''])
-                        yield pattern
+            word_count = len(name_words)
+            f_word_count = min(max(word_count // 2, 2), word_count)
+            for start in range(len(name_words) + 1 - f_word_count):
+                word_cmbs = name_words[start: start + f_word_count]
+                if sum(map(len, word_cmbs)) >= 7:
+                    for words in itertools.permutations(word_cmbs):
+                        origin_text = '_'.join(words)
+                        for i in range(len(origin_text)):
+                            new_text = ''.join([
+                                ('%' if origin_text[j] == '_' else '_') if i == j else origin_text[j]
+                                for j in range(len(origin_text))
+                            ])
+                            pattern = f'%{new_text}%'
+                            yield pattern
+                else:
+                    yield '%'.join(['', *word_cmbs, ''])
 
     __pattern_batch__: int = 200
 
@@ -292,6 +298,9 @@ class TagMatcher(HuggingfaceDeployable):
             for row in self._query_via_pattern(*patterns).get():
                 tag = row[self.__tag_column__]
                 count = row[self.__count_column__]
+                if tag in exist_tags:
+                    continue
+
                 origin_tag = tag
                 tag, count = self._alias_replace(tag, count)
                 if tag in exist_tags:
@@ -307,6 +316,7 @@ class TagMatcher(HuggingfaceDeployable):
                     continue
 
                 exist_tags.add(tag)
+                exist_tags.add(origin_tag)
                 yield tag, count
 
     def try_matching(self):
@@ -314,7 +324,9 @@ class TagMatcher(HuggingfaceDeployable):
         all_chs = self.game_cls.all(contains_extra=False)
         ch_blacklists = self.get_blacklists()
 
-        for ch in tqdm(all_chs, desc='Name Searching'):
+        ns_tqdm = tqdm(all_chs, desc='Name Searching')
+        for ch in ns_tqdm:
+            ns_tqdm.set_description(f'Name Search: {ch!r}')
             name_words_sets = [self._split_name_to_words(name) for name in ch.names]
             options = []
             for tag, count in self._yield_name_count(ch.names):
@@ -335,6 +347,7 @@ class TagMatcher(HuggingfaceDeployable):
 
             options = sorted(options, key=lambda x: (0 if x[3] else 1, -x[2], -x[1], len(x[0]), x[0]))
             ch_options[ch.index] = options
+            logging.info(f'Selected tags for {ch!r}: {options!r}')
 
         retval = []
         for ch in tqdm(all_chs, desc='Tag Matching'):
