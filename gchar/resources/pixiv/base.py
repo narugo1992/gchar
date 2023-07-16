a4d60130a17b1de45014a6d32f2a52b172d1edfe
narugo1992 <narugo@126.com>
2023-07-16 13:31:36 +0800

    dev(narugo): update nc's pixiv search name, ci skip

--- gchar/resources/pixiv/base.py
+++ gchar/resources/pixiv/base.py
@@ -4,7 +4,7 @@ _GAMES = {
     'azurlane': ('アズールレーン', 'アズールレーン'),
     'genshin': ('原神', '原神'),
     'girlsfrontline': ('ドールズフロントライン', 'ドールズフロントライン'),
-    'neuralcloud': ('云图计划', '云图计划'),
+    'neuralcloud': ('ニューラルクラウド', 'ニューラルクラウド'),
     'bluearchive': ('ブルーアーカイブ', 'ブルーアーカイブ'),
     'pathtonowhere': ('無期迷途', '無期迷途'),
     'nikke': ('勝利の女神:NIKKE', '勝利の女神:NIKKE'),
