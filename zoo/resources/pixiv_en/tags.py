88946daeeff1aff2818494413afd995def296a49
narugo1992 <narugo@126.com>
2023-08-12 12:30:33 +0800

    dev(narugo): add translation of tags, ci skip

--- zoo/resources/pixiv_en/tags.py
+++ zoo/resources/pixiv_en/tags.py
@@ -1,5 +1,10 @@
-from typing import Tuple
+from concurrent.futures import ThreadPoolExecutor
+from typing import Tuple, List, Mapping, Any
+from urllib.parse import urljoin
 
+from pyquery import PyQuery as pq
+
+from gchar.utils import srequest
 from ..pixiv.tags import PixivTagCrawler
 
 
@@ -14,3 +19,27 @@ class PixivEnTagCrawler(PixivTagCrawler):
     }
 
     __mark_tags__: Tuple[str, str, str, str] = ('Updated', 'View count', 'Submitted works', 'Checklist')
+
+    def get_tags_json(self) -> List[Mapping[str, Any]]:
+        json_data = PixivTagCrawler.get_tags_json(self)
+        retval = []
+
+        def _get_info(idx, item):
+            wiki_url = item['wiki_url']
+            resp = srequest(self.session, 'GET', wiki_url)
+            page = pq(resp.text)
+            for trans_item in page('#article-relation .interlang li'):
+                lang = trans_item('a').attr('lang')
+                item[f'trans_{lang}'] = trans_item('a').attr('gtm-id')
+                item[f'trans_{lang}_wiki_url'] = urljoin(wiki_url, trans_item('a').attr('href'))
+
+            retval.append((idx, item))
+
+        tp = ThreadPoolExecutor(max_workers=self.__max_workers__)
+        for i, item in enumerate(json_data):
+            tp.submit(_get_info, i, item)
+        tp.shutdown()
+
+        retval = sorted(retval)
+        retval = [item for _, item in retval]
+        return retval
