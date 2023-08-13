774eb117fb8b85ab62c0be03c2cef87e6720e48e
narugo1992 <narugo@126.com>
2023-08-13 22:55:42 +0800

    dev(narugo): add 2 series, ci skip

--- gchar/generic/__init__.py
+++ gchar/generic/__init__.py
@@ -1,5 +1,7 @@
+from .demonslayer import Character as KimetsuNoYaibaCharacter
 from .lovelive import Character as LoveLiveCharacter
 from .lovelivesunshine import Character as LoveLiveSunshineCharacter
+from .mahoushoujomadokamagica import Character as MahouShoujoMadokaMagicaCharacter
 from .naruto import Character as NarutoCharacter
 from .pokemon import Character as PokemonCharacter
 from .touhou import Character as TouhouCharacter
