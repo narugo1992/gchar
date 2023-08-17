11160d94c92312414def64527a0992d3803d9ef9
narugo1992 <narugo@126.com>
2023-08-17 19:39:18 +0800

    dev(narugo): add honkai3, ci skip

--- gchar/generic/__init__.py
+++ gchar/generic/__init__.py
@@ -1,19 +1,20 @@
-from .kantaicollection import Character as KantaiCollectionCharacter
-from .spyxfamily import Character as SpyFamilyCharacter
-from .isitwrongtotrytopickupgirlsinadungeon import \
-    Character as DungeonNiDeaiWoMotomeruNoWaMachigatteiruNoDarouKaCharacter
-from .toarumajutsunoindex import Character as ToAruMajutsuNoIndexCharacter
-from .konosuba import Character as KonoSubarashiiSekaiNiShukufukuWoCharacter
 from .bangdream import Character as BangDreamCharacter
 from .demonslayer import Character as KimetsuNoYaibaCharacter
 from .eromangasensei import Character as EromangaSenseiCharacter
+from .honkai3 import Character as Houkai3rdCharacter
+from .isitwrongtotrytopickupgirlsinadungeon import \
+    Character as DungeonNiDeaiWoMotomeruNoWaMachigatteiruNoDarouKaCharacter
+from .kantaicollection import Character as KantaiCollectionCharacter
+from .konosuba import Character as KonoSubarashiiSekaiNiShukufukuWoCharacter
 from .lovelive import Character as LoveLiveCharacter
 from .lovelivesunshine import Character as LoveLiveSunshineCharacter
 from .mahoushoujomadokamagica import Character as MahouShoujoMadokaMagicaCharacter
 from .naruto import Character as NarutoCharacter
 from .pokemon import Character as PokemonCharacter
 from .rezero import Character as ReZeroKaraHajimeruIsekaiSeikatsuCharacter
+from .spyxfamily import Character as SpyFamilyCharacter
 from .swordartonline import Character as SwordArtOnlineCharacter
+from .toarumajutsunoindex import Character as ToAruMajutsuNoIndexCharacter
 from .touhou import Character as TouhouCharacter
 from .violetevergarden import Character as VioletEvergardenCharacter
 from .vocaloid import Character as VocaloidCharacter
