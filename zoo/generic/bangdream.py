8ccbf9c970687f6cf0ad2731a1978bb6e6db8950
narugo1992 <narugo@126.com>
2023-09-25 21:05:44 +0800

    dev(narugo): more root names

--- zoo/generic/bangdream.py
+++ zoo/generic/bangdream.py
@@ -5,10 +5,27 @@ class Indexer(ZerochanBasedIndexer):
     __game_name__ = 'bangdream'
     __official_name__ = 'BanG Dream!'
     __root_website__ = 'https://zerochan.net/BanG+Dream%21'
+    __root_names__ = [
+        "BanG Dream! Girls Band Party!",
+        "BanG Dream!",
+        "BanG Dream! Dai 2-ki",
+        "BanG Dream! Girls Band Party! PICO",
+        "BanG Dream! 3rd Season",
+        "Bang Dream! Girls Band Party! PICO ~OHMORI~",
+        "BanG Dream! Episode of Roselia I: Yakusoku",
+        "Bang Dream! Girls Band Party! PICO: Fever!",
+        "BanG Dream! Episode of Roselia II: Song I am",
+        "BanG Dream! FILM LIVE 2nd Stage",
+        "Bang Dream! FILM LIVE",
+        "BanG Dream! Poppin Dream!",
+        "BanG Dream! 11th☆LIVE",
+        "BanG Dream! Special☆LIVE Girls Band Party! 2020",
+        "BanG Dream! Special☆LIVE Girls Band Party! 2020→2022",
+        "Bang Dream! 2nd Illustration Contest"
+    ]
 
 
 INDEXER = Indexer()
 
 if __name__ == '__main__':
     INDEXER.get_cli()()
-
--- zoo/generic/shoujokagekirevuestarlight.py
+++ zoo/generic/shoujokagekirevuestarlight.py
@@ -5,10 +5,15 @@ class Indexer(ZerochanBasedIndexer):
     __game_name__ = 'shoujokagekirevuestarlight'
     __official_name__ = 'Shoujo☆Kageki Revue Starlight'
     __root_website__ = 'https://zerochan.net/Shoujo%E2%98%86Kageki+Revue+Starlight'
+    __root_names__ = [
+        "Shoujo☆Kageki Revue Starlight -ReLIVE-",
+        "Shoujo☆Kageki Revue Starlight",
+        "Gekijouban Shoujo☆Kageki Revue Starlight",
+        "Shoujo☆Kageki Revue Starlight: Rondo Rondo Rondo"
+    ]
 
 
 INDEXER = Indexer()
 
 if __name__ == '__main__':
     INDEXER.get_cli()()
-
