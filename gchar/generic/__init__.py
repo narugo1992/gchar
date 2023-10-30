f20cd8993d4e2f3117c094e9993ee808cba81578
narugo1992 <narugo992@gmail.com>
2023-10-30 13:07:00 +0800

    dev(narugo): add fireemblem, ci skip

--- gchar/generic/__init__.py
+++ gchar/generic/__init__.py
@@ -1,35 +1,36 @@
-from .shoujokagekirevuestarlight import Character as ShoujoKagekiRevueStarlightCharacter
-from .theidolmstermillionlive import Character as TheIdolmSterMillionLiveCharacter
-from .bangdreamdai2ki import Character as BangDreamDai2KiCharacter
-from .idolmastercinderellagirls import Character as TheIdolmSterCinderellaGirlsCharacter
-from .theidolmstershinycolors import Character as TheIdolmSterShinyColorsCharacter
-from .lovelivesuperstar import Character as LoveLiveSuperstarCharacter
-from .theidolmster import Character as TheIdolmSterCharacter
-from .lovelivenijigasakihighschoolidolclub import Character as LoveLiveNijigasakiGakuenSchoolIdolDoukoukaiCharacter
-from .loveliveschoolidolfestivalallstars import Character as LoveLiveSchoolIdolFestivalAllStarsCharacter
-from .warshipgirls import Character as ZhanJianShaoNyuCharacter
 from .arknightsnpc import Character as ArknightsCharacter
 from .bangdream import Character as BangDreamCharacter
+from .bangdreamdai2ki import Character as BangDreamDai2KiCharacter
 from .demonslayer import Character as KimetsuNoYaibaCharacter
 from .eromangasensei import Character as EromangaSenseiCharacter
+from .fireemblem import Character as FireEmblemCharacter
 from .genshinnpc import Character as GenshinImpactCharacter
 from .honkai3 import Character as Houkai3rdCharacter
+from .idolmastercinderellagirls import Character as TheIdolmSterCinderellaGirlsCharacter
 from .isitwrongtotrytopickupgirlsinadungeon import \
     Character as DungeonNiDeaiWoMotomeruNoWaMachigatteiruNoDarouKaCharacter
 from .kantaicollection import Character as KantaiCollectionCharacter
 from .konosuba import Character as KonoSubarashiiSekaiNiShukufukuWoCharacter
 from .lovelive import Character as LoveLiveCharacter
+from .lovelivenijigasakihighschoolidolclub import Character as LoveLiveNijigasakiGakuenSchoolIdolDoukoukaiCharacter
+from .loveliveschoolidolfestivalallstars import Character as LoveLiveSchoolIdolFestivalAllStarsCharacter
 from .lovelivesunshine import Character as LoveLiveSunshineCharacter
+from .lovelivesuperstar import Character as LoveLiveSuperstarCharacter
 from .mahoushoujomadokamagica import Character as MahouShoujoMadokaMagicaCharacter
 from .naruto import Character as NarutoCharacter
 from .pokemon import Character as PokemonCharacter
 from .rezero import Character as ReZeroKaraHajimeruIsekaiSeikatsuCharacter
+from .shoujokagekirevuestarlight import Character as ShoujoKagekiRevueStarlightCharacter
 from .spyxfamily import Character as SpyFamilyCharacter
 from .swordartonline import Character as SwordArtOnlineCharacter
+from .theidolmster import Character as TheIdolmSterCharacter
+from .theidolmstermillionlive import Character as TheIdolmSterMillionLiveCharacter
+from .theidolmstershinycolors import Character as TheIdolmSterShinyColorsCharacter
 from .toarumajutsunoindex import Character as ToAruMajutsuNoIndexCharacter
 from .touhou import Character as TouhouCharacter
 from .violetevergarden import Character as VioletEvergardenCharacter
 from .vocaloid import Character as VocaloidCharacter
+from .warshipgirls import Character as ZhanJianShaoNyuCharacter
 from .yugioh import Character as YuGiOhCharacter
 
 
